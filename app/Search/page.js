2292d9c45399b1ae69e1afe40bd7e8503a07aecf
BOOK-7CBHGOLK4N\ii672 <ii6725@naver.com>
2024-03-13 11:01:17 +0900

    부분추출 저장

--- app/Search/page.js
+++ app/Search/page.js
@@ -4,30 +4,34 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const StocksPage = () => {
-  const [stockData, setStockData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [filteredData, setFilteredData] = useState([]);
-  const [selectedDate, setSelectedDate] = useState('');
+  const [stockData, setStockData] = useState([]); // 주식 데이터 상태 변수
+  const [loading, setLoading] = useState(true); // 로딩 상태 변수
+  const [searchQuery, setSearchQuery] = useState(''); // 검색어 상태 변수
+  const [filteredData, setFilteredData] = useState([]); // 필터링된 데이터 상태 변수
+  const [selectedDate, setSelectedDate] = useState(''); // 선택된 날짜 상태 변수
 
+  // 선택된 날짜가 변경될 때마다 주식 데이터를 가져오는 함수 호출
   useEffect(() => {
     if (selectedDate) {
       fetchData(selectedDate);
     }
   }, [selectedDate]);
 
+  // 선택된 날짜로 주식 데이터를 가져오는 비동기 함수
   const fetchData = async (targetDate) => {
     try {
       const response = await axios.get(`/api/stock/isu?date=${targetDate}`);
-      setStockData(response.data.OutBlock_1);
-      setLoading(false);
-
+      // 서버에서 받아온 데이터 중 필요한 부분만 추출하여 저장
+      const extractedData = response.data.OutBlock_1;
+      setStockData(extractedData);
+      setLoading(false); // 로딩 상태 변경
     } catch (error) {
       console.error('Error fetching data:', error);
-      setLoading(false);
+      setLoading(false); // 로딩 상태 변경
     }
   };
 
+  // 검색어가 변경될 때마다 필터링된 데이터 업데이트
   useEffect(() => {
     if (searchQuery === '') {
       setFilteredData(stockData);
@@ -39,10 +43,12 @@ const StocksPage = () => {
     }
   }, [searchQuery, stockData]);
 
+  // 검색어 입력 핸들러
   const handleSearchChange = event => {
     setSearchQuery(event.target.value);
   };
 
+  // 날짜 선택 핸들러
   const handleDateChange = event => {
     setSelectedDate(event.target.value);
   };
@@ -80,21 +86,20 @@ const StocksPage = () => {
                   }}
                 >
                   <p className="data-value" style={{ color: 'red' }} ><strong>{stock.ISU_NM}</strong></p>
-                  <p className="data-value" >거래 기준일: {stock.BAS_DD}</p> {/* BAS_DD: -> 거래 기준일: */}
-                  <p className="data-value" >종목 코드: {stock.ISU_CD}</p> {/* ISU_CD: -> 종목 코드: */}
-                  <p className="data-value" >시장 구분: {stock.MKT_NM}</p> {/* MKT_NM: -> 시장 구분: */}
-                  <p className="data-value" >섹션 구분: {stock.SECT_TP_NM}</p> {/* SECT_TP_NM: -> 섹션 구분: */}
-                  <p className="data-value" >거래 종가: {stock.TDD_CLSPRC}</p> {/* TDD_CLSPRC: -> 거래 종가: */}
-                  <p className="data-value" >전일 대비 가격 변동: {stock.CMPPREVDD_PRC}</p> {/* CMPPREVDD_PRC: -> 전일 대비 가격 변동: */}
-                  <p className="data-value" >등락률: {stock.FLUC_RT}</p> {/* FLUC_RT: -> 등락률: */}
-                  <p className="data-value" >거래 시가: {stock.TDD_OPNPRC}</p> {/* TDD_OPNPRC: -> 거래 시가: */}
-                  <p className="data-value" >거래 고가: {stock.TDD_HGPRC}</p> {/* TDD_HGPRC: -> 거래 고가: */}
-                  <p className="data-value" >거래 저가: {stock.TDD_LWPRC}</p> {/* TDD_LWPRC: -> 거래 저가: */}
-                  <p className="data-value" >누적 거래량: {stock.ACC_TRDVOL}</p> {/* ACC_TRDVOL: -> 누적 거래량: */}
-                  <p className="data-value" >누적 거래대금: {stock.ACC_TRDVAL}</p> {/* ACC_TRDVAL: -> 누적 거래대금: */}
-                  <p className="data-value" >시가총액: {stock.MKTCAP}</p> {/* MKTCAP: -> 시가총액: */}
-                  <p className="data-value" >상장주식수: {stock.LIST_SHRS}</p> {/* LIST_SHRS: -> 상장주식수: */}
-                  {/* 기타 데이터 표시 */}
+                  <p className="data-value" >거래 기준일: {stock.BAS_DD}</p>
+                  <p className="data-value" >종목 코드: {stock.ISU_CD}</p>
+                  <p className="data-value" >시장 구분: {stock.MKT_NM}</p>
+                  <p className="data-value" >섹션 구분: {stock.SECT_TP_NM}</p>
+                  <p className="data-value" >거래 종가: {stock.TDD_CLSPRC}</p>
+                  <p className="data-value" >전일 대비 가격 변동: {stock.CMPPREVDD_PRC}</p>
+                  <p className="data-value" >등락률: {stock.FLUC_RT}</p>
+                  <p className="data-value" >거래 시가: {stock.TDD_OPNPRC}</p>
+                  <p className="data-value" >거래 고가: {stock.TDD_HGPRC}</p>
+                  <p className="data-value" >거래 저가: {stock.TDD_LWPRC}</p>
+                  <p className="data-value" >누적 거래량: {stock.ACC_TRDVOL}</p>
+                  <p className="data-value" >누적 거래대금: {stock.ACC_TRDVAL}</p>
+                  <p className="data-value" >시가총액: {stock.MKTCAP}</p>
+                  <p className="data-value" >상장주식수: {stock.LIST_SHRS}</p>
                 </div>
               ))}
             </div>
